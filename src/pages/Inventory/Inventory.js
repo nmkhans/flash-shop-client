a1e415eff4b260d3cc30a41b054f42886e36d6ad
Moin Khan <nurmoin05@gmail.com>
2022-04-29 22:05:17 +0600

    inventory management completed

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -5,6 +5,8 @@ import { useParams } from 'react-router-dom';
 const Inventory = () => {
     const [inventoryItem, setInventoryItem] = useState({});
     const [res, setRes] = useState({});
+    const [reStock, setReStock] = useState(false);
+
     const { id } = useParams();
     useEffect(() => {
         fetch(`http://localhost:5000/cars/${id}`)
@@ -15,19 +17,41 @@ const Inventory = () => {
     const { name, img, description, price, quantity, supplier } = inventoryItem;
 
     const handleDeliver = () => {
+        if (quantity > 0) {
+            const newQuantity = {
+                quantity: (quantity - 1)
+            };
+
+            const url = `http://localhost:5000/cars/${id}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newQuantity),
+            })
+                .then(res => res.json())
+                .then(data => setRes(data));
+        }
+    }
+
+    const HandleForm = (event) => {
+        event.preventDefault();
+        const amount = parseInt(event.target.amount.value);
         const newQuantity = {
-            quantity: (quantity - 1)
-        };
+            quantity: (quantity + amount),
+        }
+        console.log(newQuantity)
         const url = `http://localhost:5000/cars/${id}`;
         fetch(url, {
             method: 'PUT',
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(newQuantity),
+            body: JSON.stringify(newQuantity)
         })
         .then(res => res.json())
-        .then(data => setRes(data));
+        .then(data => setRes(data))
     }
 
     return (
@@ -41,15 +65,27 @@ const Inventory = () => {
                         <div className="inventory__detail">
                             <h3>{name}</h3>
                             <p className="inventory__detail__price">${price}</p>
-                            <p className="inventory__detail__stock">In stoke: {quantity}</p>
+                            <p className="inventory__detail__stock">In stoke: {(quantity !== 0) ? quantity : "Out of stock"}</p>
                             <p className="inventory__detail__brand">Brand: {supplier}</p>
                             <p className="inventory__detail__desc">{description}</p>
                             <div className="inventory__button">
                                 <button onClick={handleDeliver}>Delivered</button>
-                                <button>Restock</button>
+                                <button onClick={() => setReStock(!reStock)}>Restock</button>
                             </div>
                         </div>
                     </div>
+                    {reStock && (
+                        <div className="inventory__restock">
+                            <form onSubmit={HandleForm}>
+                                <div className="inventory__restock__field">
+                                    <input type="number" name="amount" placeholder="Enter Amount" />
+                                </div>
+                                <div className="inventory__restock__field">
+                                    <input type="submit" value="Submit" />
+                                </div>
+                            </form>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
